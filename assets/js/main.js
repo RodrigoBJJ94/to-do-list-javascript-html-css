64ea4597d7e7d3f6e47dee79c2d2d4e14e28c702
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-06-03 14:14:03 -0300

    Versão 1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,34 +3,34 @@ const buttonAddTask = document.querySelector('.button-add-task');
 const tasks = document.querySelector('.tasks');
 
 function createLi() {
-    const li = document.createElement('li'); // Criar uma lista HTML pelo Javascript
+    const li = document.createElement('li'); 
     return li;
 }
 
 inputNewTask.addEventListener('keypress', function(event) {
-    if (event.keyCode === 13) { // Quando o botão enter for pressionado
-        if (!inputNewTask.value) return; // Quando nada for digitado
+    if (event.keyCode === 13) { 
+        if (!inputNewTask.value) return; 
         createTask(inputNewTask.value);
     }
 });
 
 function clearInput() {
-    inputNewTask.value = ''; // Apagar sempre a escrita anterior
-    inputNewTask.focus(); // Cursor ficar piscando
+    inputNewTask.value = '';
+    inputNewTask.focus();
 }
 
 function createButtonErase(li) { 
     li.innerHTML += ' ';
-    const buttonErase = document.createElement('button'); // Criar botão de apagar
+    const buttonErase = document.createElement('button'); 
     buttonErase.innerHTML = 'Apagar';
     buttonErase.setAttribute('class', 'apagar') 
-    li.appendChild(buttonErase); // buttonErase é filho de li
+    li.appendChild(buttonErase); 
 }
 
 function createTask(textInput) {
     const li = createLi();
     li.innerHTML = textInput;
-    tasks.appendChild(li); // li é filha de tasks
+    tasks.appendChild(li);
     clearInput();
     createButtonErase(li);
     salveTasks();
@@ -43,28 +43,28 @@ buttonAddTask.addEventListener('click', function() {
 
 document.addEventListener('click', function(event) {
     const element = event.target;
-    if (element.classList.contains('apagar')) { // Selecionar o botão apagar
-        element.parentElement.remove(); // Remover o pai/mãe do botão apagar e ele mesmo
+    if (element.classList.contains('apagar')) { 
+        element.parentElement.remove(); 
         salveTasks();
     }
 })
 
 function salveTasks() {
-    const liTasks = tasks.querySelectorAll('li'); // Selecionar todos os li
+    const liTasks = tasks.querySelectorAll('li'); 
     const listOfTasks = [];
 
     for (let task of liTasks) {
         let taskText = task.innerText;
-        taskText = taskText.replace('Apagar', '').trim(); // Remover o apagar escrito
+        taskText = taskText.replace('Apagar', '').trim(); 
         listOfTasks.push(taskText);
     }
-    const tasksJSON = JSON.stringify(listOfTasks); // Converter o array em string
-    localStorage.setItem('tasks', tasksJSON); // Salvar as strings no storage
-}   // As conversões de JSON são para página não perder os dados salvos se for fechada
+    const tasksJSON = JSON.stringify(listOfTasks); 
+    localStorage.setItem('tasks', tasksJSON); 
+}   
 
 function addTasksSalve() {
     const tasks = localStorage.getItem('tasks');
-    const listOfTasks = JSON.parse(tasks); // Converter de volta a string em array
+    const listOfTasks = JSON.parse(tasks); 
 
     for (let task of listOfTasks) {
         createTask(task);
